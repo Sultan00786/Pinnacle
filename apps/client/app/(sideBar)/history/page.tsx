ff1276d8573c5156efda12db9d10ff7fd85fb594
SultanAliJavedAli <treeloversultan0987@gmail.com>
2025-01-16 16:04:36 +0530

    feat: table commponent added in transaction history pages

--- apps/client/app/(sideBar)/history/page.tsx
+++ apps/client/app/(sideBar)/history/page.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
+import TableComponent from "../../../components/transaction/TableComponent";
+import getTransaction from "../../lib/support/getTransaction";
+import { toast } from "react-toastify";
+import { TransactionType } from "@repo/interface/interface";
+import { Loader } from "@repo/ui/component";
+import { fuctionTxs } from "../../../lib/fetchTransaction";
 
 function TransactionHistrory() {
-  return (
-    <div>TransactionHistrory</div>
-  )
+   const [loading, setLoading] = useState(false);
+   const [tableData, setTableData] = useState<TransactionType[]>(
+      [] as TransactionType[]
+   );
+
+   // const fuctionTxs = async () => {
+   //    setLoading(true);
+   //    const toastId = toast.loading("Please wait...");
+   //    console.log("hellow ");
+   //    const txs = await getTransaction();
+   //    console.log(txs);
+   //    if (txs.success && txs.transactions) {
+   //       setTableData(txs.transactions);
+   //       toast.update(toastId, {
+   //          render: "Transaction History",
+   //          type: "success",
+   //          isLoading: false,
+   //          autoClose: 6000,
+   //       });
+   //    }
+   //    setLoading(false);
+   // };
+
+   useEffect(() => {
+      fuctionTxs(setLoading, setTableData);
+   }, []);
+
+   if (loading) {
+      return (
+         <div>
+            <Loader />
+         </div>
+      );
+   }
+
+   return (
+      <div>
+         <TableComponent table_data={tableData} />
+      </div>
+   );
 }
 
-export default TransactionHistrory
+export default TransactionHistrory;
